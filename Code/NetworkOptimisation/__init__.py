adf5810a558dc8ab06f25eada1c6bbac7fc7f521
MatthewPirotta <matthew.pirotta.23@um.edu.mt>
2026-04-29 22:26:51 +0200

    and this

--- Code/NetworkOptimisation/__init__.py
+++ Code/NetworkOptimisation/__init__.py
@@ -1,3 +1,3 @@
-from .graph_optimisation_heuristic import run_global
+from .graph_optimisation_heuristic import run_optimisation
 from .heuristic import *
 from .evaluation import network_evaluation
